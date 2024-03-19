807899e9ddf62c24c1485b36f342e7e2b6e86803
Leonardo Monteiro Huck <leonardomonteirohuck@gmail.com>
2024-03-19 19:12:02 -0300

    Criacao da classe Pessoa

---
+++ pooMaiorIdade/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMaiorIdade
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+			this.Nome = "";
+			this.Idade = 0;
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+			this.Idade = idade;
+        }
+
+        private string nome;
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		private int idade;
+
+		public int Idade
+		{
+			get { return idade; }
+			set {
+				if (value > 0)
+					this.idade = value;
+				else
+					this.idade = 0;
+			}
+		}
+
+
+		public void ExibirDados()
+		{
+			Console.WriteLine($"Nome: {this.Nome}");
+			Console.WriteLine($"Idade: {this.Idade}");
+		}
+	}
+}
