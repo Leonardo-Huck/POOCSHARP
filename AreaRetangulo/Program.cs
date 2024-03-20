dfa9d0e5036f0b6888949e8b3cc5cf2faf0bc2a9
Leonardo Monteiro Huck <leonardomonteirohuck@gmail.com>
2024-03-20 18:46:16 -0300

    Exercicio calculo da area de um retangulo/quadrado

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AreaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Calculo da area de um retangulo ou quadrado");
+            Console.Write("Informe a base: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe a altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+
+            retangulo.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AreaRetangulo/Retangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        private string tipoObjeto = "Retangulo";
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public void ExibirDados()
+        {
+            if(Base == Altura)
+            {
+                this.tipoObjeto = "Quadrado";
+            }
+            Console.WriteLine($"A base do {tipoObjeto} é de: {Base}");
+            Console.WriteLine($"A altura do {tipoObjeto} é de: {Altura}");
+            Console.WriteLine($"A Área do {tipoObjeto} é de: {Base*Altura}");
+        }
+            
+    }
+}
